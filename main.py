77f72e8fca79dc85b8032b68fe55dd046fc116ed
debaser19 <steve@stoveld.xyz>
2022-03-15 13:15:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -90,6 +90,7 @@ def get_matchup_sheet():
 @bot.event
 async def add_reaction(message):
     rigged_emote = '<:RiggedGNL:833837273081577472>'
+    print(message.content)
     if 'rigged' in message.content.lower():
         print(f'Reacting with Rigged emote to {message.author}')
         await message.add_reaction(rigged_emote)
